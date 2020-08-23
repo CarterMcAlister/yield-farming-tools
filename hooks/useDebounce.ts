5a8866a5ca31936f4ebbd068ca2dc5da90720951
Carter <carter@carters-mbp.home>
2020-08-23 02:38:38 -0700

    Create impermanent loss calculator

---
+++ hooks/useDebounce.ts
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react'
+
+export function useDebounce(value, delay = 100) {
+  const [debouncedValue, setDebouncedValue] = useState(value)
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value)
+    }, delay)
+
+    return () => {
+      clearTimeout(handler)
+    }
+  }, [value])
+
+  return debouncedValue
+}
